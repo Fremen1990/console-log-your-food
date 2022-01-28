98269303e13d075bbbed256d1ce594bc3b155bda
Fremen1990 <fremen1990@gmail.com>
2022-01-29 00:14:10 +0100

    get from database vegan and all foods

--- index.js
+++ index.js
@@ -3,24 +3,64 @@
 const axios = require("axios");
 
 const readline = require('readline').createInterface({
-    input:process.stdin,
-    output:process.stdout,
-})
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'enter command >'
+});
 
-readline.question("What would you like to log today?  \n", async (item)=>{
-    const {data} = await axios.get(`http://localhost:3000/food`)
-    const it = data[Symbol.iterator]();
-    let position = it.next();
+readline.prompt();
+readline.on('line', line => {
+    switch (line.trim()) {
 
-    while(!position.done){
-        const food = position.value.name;
-        if(food===item){
-            console.log(`${item} has ${position.value.calories} calories`)
+
+        case 'list vegan foods': {
+            axios.get(`http://localhost:3000/food`).then(({data}) => {
+                let idx = 0;
+                const veganOnly = data.filter(food =>
+                    food.dietary_preferences.includes('vegan'),
+                );
+                const veganIterable = {
+                    [Symbol.iterator]() {
+                        return {
+                            [Symbol.iterator]() {
+                                return this;
+                            },
+                            next() {
+                                const current = veganOnly[idx];
+                                idx++;
+                                if (current) {
+                                    return {value: current, done: false};
+                                } else {
+                                    return {value: current, done: true};
+                                }
+                            },
+                        };
+                    },
+                };
+                for (let val of veganIterable) {
+                    console.log(val.name);
+                }
+                readline.prompt();
+            });
         }
-        position= it.next();
+            break;
+
+
+        case 'log': {
+            readline.question("What would you like to log today?  \n", async (item) => {
+                const {data} = await axios.get(`http://localhost:3000/food`)
+                const it = data[Symbol.iterator]();
+                let position = it.next();
+
+                while (!position.done) {
+                    const food = position.value.name;
+                    if (food === item) {
+                        console.log(`${item} has ${position.value.calories} calories`)
+                    }
+                    position = it.next();
+                }
+                readline.prompt();
+            })
+        }
     }
-
-    readline.close();
 })
-
-
