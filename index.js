eb41789827440456902abdb8bb3efdd189bbecca
Fremen1990 <fremen1990@gmail.com>
2022-01-30 00:23:23 +0100

    logging eaten food and saving in JSON database fininshed

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 //Database json-server run: json-server --watch ./db.json
 
-const axios = require("axios");
-
+const axios = require('axios');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: 'enter command >'
+    prompt: 'enter command > ',
 });
-
 readline.prompt();
 readline.on('line', async line => {
     switch (line.trim()) {
-
-
-        case 'list vegan foods': {
-            axios.get(`http://localhost:3000/food`).then(({data}) => {
+        case 'list vegan foods':
+        {
+            axios.get(`http://localhost:3000/food`).then(({ data }) => {
                 let idx = 0;
                 const veganOnly = data.filter(food =>
                     food.dietary_preferences.includes('vegan'),
@@ -29,9 +26,9 @@ readline.on('line', async line => {
                                 const current = veganOnly[idx];
                                 idx++;
                                 if (current) {
-                                    return {value: current, done: false};
+                                    return { value: current, done: false };
                                 } else {
-                                    return {value: current, done: true};
+                                    return { value: current, done: true };
                                 }
                             },
                         };
@@ -44,39 +41,105 @@ readline.on('line', async line => {
             });
         }
             break;
-
-
-        case 'log': {
-            const {data} = await axios.get(`http://localhost:3000/food`)
+        case 'log':
+            const { data } = await axios.get(`http://localhost:3000/food`);
             const it = data[Symbol.iterator]();
+            let actionIt;
 
             const actionIterator = {
                 [Symbol.iterator]() {
-                    const positions = [...this.actions];
+                    let positions = [...this.actions];
                     return {
                         [Symbol.iterator]() {
                             return this;
                         },
-                        next()
+                        next(...args) {
+                            if (positions.length > 0) {
+                                const position = positions.shift();
+                                const result = position(...args);
+                                return { value: result, done: false };
+                            } else {
+                                return { done: true };
+                            }
+                        },
+                        return() {
+                            positions = [];
+                            return { done: true };
+                        },
+                        throw(error) {
+                            console.log(error);
+                            return { value: undefined, done: true };
+                        },
+                    };
+                },
+                actions: [askForServingSize, displayCalories],
+            };
+
+        function askForServingSize(food) {
+            readline.question(
+                `How many servings did you eat? ( as a decimal: 1, 0.5, 1.25, etc.. ) `,
+                servingSize => {
+                    if (servingSize === 'nevermind' || servingSize === 'n') {
+                        actionIt.return();
+                    } else {
+                        actionIt.next(servingSize, food);
                     }
                 },
-                actions:[askForServingSize, displayCalories],
-            }
+            );
+        }
 
-            readline.question("What would you like to log today?  \n", async (item) => {
+        async function displayCalories(servingSize = 1, food) {
+            const calories = food.calories;
+            console.log(
+                `${
+                    food.name
+                } with a serving size of ${servingSize} has ${Number.parseFloat(
+                    calories * parseInt(servingSize, 10),
+                ).toFixed()} calories.`,
+            );
+            const { data } = await axios.get(`http://localhost:3000/users/1`);
+            const usersLog = data.log || [];
+            const putBody = {
+                ...data,
+                log: [
+                    ...usersLog,
+                    {
+                        [Date.now()]: {
+                            food: food.name,
+                            servingSize,
+                            calories: Number.parseFloat(
+                                calories * parseInt(servingSize, 10),
+                            ),
+                        },
+                    },
+                ],
+            };
+            await axios.put(`http://localhost:3000/users/1`, putBody, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            actionIt.next();
+            readline.prompt();
+        }
+
+            readline.question(`What would you like to log today? `, item => {
                 let position = it.next();
-
                 while (!position.done) {
                     const food = position.value.name;
                     if (food === item) {
-                        console.log(`${item} has ${position.value.calories} calories`)
+                        console.log(
+                            `A single serving of ${item} has ${position.value.calories} calories.`,
+                        );
+                        actionIt = actionIterator[Symbol.iterator]();
+                        actionIt.next(position.value);
                     }
                     position = it.next();
                 }
                 readline.prompt();
-            })
-        }
+            });
+            break;
     }
-})
-
-
+    readline.prompt();
+});
